f94b3999ec2369056990cfef134b0a364cc553bd
Julian Adler <julian.dot.adler@gmail.com>
2023-07-02 02:29:05 +0000

    derpherp

--- .devcontainer/scripts/rc.sh
+++ .devcontainer/scripts/rc.sh
@@ -2,6 +2,7 @@ alias yolo='git aa && git commit -m "$(curl -s https://whatthecommit.com/index.t
 alias ls="exa --git"
 alias lla="exa --git"
 
+# Add your shell preferences in .devcontainer/scripts/custom.sh
 if [[ -f /home/node/scripts/custom.sh ]]; then
     source /home/node/scripts/custom.sh
 fi
