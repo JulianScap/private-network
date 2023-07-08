d37ddaf486ab633794a1c5a14cca5874ba34370d
Julian <julian.dot.adler@gmail.com>
2023-07-08 18:33:59 +1200

    Reticulating splines...

--- src/server/middlewares/authCheck.js
+++ src/server/middlewares/authCheck.js
@@ -1,6 +1,8 @@
 import { validateToken } from '../auth/validateToken.js';
+import Logger from '../common/Logger.js';
 
 export const authCheck = (ctx, next) => {
+  Logger.info('Validating token');
   const { request } = ctx;
   const { header } = request;
   const { authorization } = header;
